1239522dd0a5e52bd8d4ca87a648e34b85372296
Lira Guedes <liracrg@gmail.com>
2024-03-09 23:24:15 -0300

    feat: Cria template para Profile

--- projects/views.py
+++ projects/views.py
@@ -1,11 +1,25 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from projects.models import Profile, Project
 from projects.serializers import ProfileSerializer, ProjectSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.shortcuts import render
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+
+    def get_permissions(self):
+        if self.request.method == "GET":
+            return [permissions.AllowAny()]
+        return [permissions.IsAuthenticated()]
+
+    def retrieve(self, request, *args, **kwargs):
+        if self.request.method == "GET":
+            profile_id = self.kwargs.get("pk")
+            profile = Profile.objects.get(id=profile_id)
+            return render(request, "profile_detail.html", {"profile": profile})
+        return super().retrieve(request, *args, **kwargs)
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
